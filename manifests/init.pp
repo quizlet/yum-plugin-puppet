9424c1ded2070bd2f221123a57bbe5c806d8563d
Robert Deaton <robert@quizlet.com>
2014-03-26 12:55:06 -0700

    Fix naming issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
-class puppet-yum-plugin {
+class yum-puppet-plugin {
     file { "/etc/yum/pluginconf.d/puppet.conf":
         mode    => 0644,
         owner   => root,
         group   => root,
         ensure  => file,
-        source  => "puppet:///modules/yum/puppet.conf",
+        source  => "puppet:///modules/yum-puppet-plugin/puppet.conf",
     }
     file { "/usr/lib/yum-plugins/puppet.py":
         mode    => 0644,
         owner   => root,
         group   => root,
         ensure  => file,
-        source  => "puppet:///modules/yum/puppet.py",
+        source  => "puppet:///modules/yum-puppet-plugin/puppet.py",
     }
 }
