69468db04998afcb52a7bc87947e60e69549f5a2
Ryan Gordon <ryan@quizlet.com>
2015-02-06 13:13:49 -0800

    file mode is now required to be strings instead of integer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class yum_puppet_plugin {
     file { "/etc/yum/pluginconf.d/puppet.conf":
-        mode    => 0644,
+        mode    => '0644',
         owner   => root,
         group   => root,
         ensure  => file,
         source  => "puppet:///modules/yum_puppet_plugin/puppet.conf",
     }
     file { "/usr/lib/yum-plugins/puppet.py":
-        mode    => 0644,
+        mode    => '0644',
         owner   => root,
         group   => root,
         ensure  => file,
